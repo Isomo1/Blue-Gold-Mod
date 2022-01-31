34afcd061d08b0292b364818132f29e7a53a225f
Isomo1 <Igor.S.L@outlook.com>
2022-01-31 04:13:22 -0300

    added a block

--- src/main/java/net/isomo/isomosmod/IsomosMod.java
+++ src/main/java/net/isomo/isomosmod/IsomosMod.java
@@ -1,6 +1,7 @@
 package net.isomo.isomosmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.isomo.isomosmod.block.ModBlocks;
 import net.isomo.isomosmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,6 @@ public class IsomosMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/isomo/isomosmod/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package net.isomo.isomosmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.isomo.isomosmod.IsomosMod;
+import net.isomo.isomosmod.item.ModItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+
+    public static final Block BLUE_GOLD_BLOCK = registerBlock("blue_gold_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.BLUEGOLD);
+
+    private static Block registerBlock(String name, Block block, ItemGroup group){
+        registerBlockItem(name,block,group);
+        return Registry.register(Registry.BLOCK, new Identifier(IsomosMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem (String name, Block block, ItemGroup group){
+        return Registry.register(Registry.ITEM, new Identifier(IsomosMod.MOD_ID, name)
+        , new BlockItem(block, new FabricItemSettings().group(group)));
+    }
+
+    public static void registerModBlocks(){
+        IsomosMod.LOGGER.info("Registering ModBlocks for "+IsomosMod.MOD_ID);
+    }
+}
