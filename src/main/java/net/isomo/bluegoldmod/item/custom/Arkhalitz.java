dded33b4864ed861e135eef4f37e873cd4d39d7d
Isomo1 <Igor.S.L@outlook.com>
2022-03-05 12:59:55 -0300

    tweaks

--- src/main/java/net/isomo/bluegoldmod/item/custom/Arkhalitz.java
+++ src/main/java/net/isomo/bluegoldmod/item/custom/Arkhalitz.java
@@ -11,30 +11,30 @@ import com.google.common.collect.ImmutableMultimap.Builder;
 import net.minecraft.block.*;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.attribute.EntityAttributeModifier.Operation;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.property.Properties;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Random;
 
-public class Arkhalitz extends ToolItem implements Vanishable {
+public class Arkhalitz extends ToolItem {
     private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
 
     public Arkhalitz(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
@@ -46,37 +46,12 @@ public class Arkhalitz extends ToolItem implements Vanishable {
         this.attributeModifiers = builder.build();
     }
 
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
-        BlockState blockState = world.getBlockState(blockPos);
-        boolean bl = false;
-        if (!CampfireBlock.canBeLit(blockState) && !CandleBlock.canBeLit(blockState) && !CandleCakeBlock.canBeLit(blockState)) {
-            blockPos = blockPos.offset(context.getSide());
-            if (AbstractFireBlock.canPlaceAt(world, blockPos, context.getPlayerFacing())) {
-                this.playUseSound(world, blockPos);
-                world.setBlockState(blockPos, AbstractFireBlock.getState(world, blockPos));
-                world.emitGameEvent(context.getPlayer(), GameEvent.BLOCK_PLACE, blockPos);
-                bl = true;
-            }
-        } else {
-            this.playUseSound(world, blockPos);
-            world.setBlockState(blockPos, (BlockState)blockState.with(Properties.LIT, true));
-            world.emitGameEvent(context.getPlayer(), GameEvent.BLOCK_PLACE, blockPos);
-            bl = true;
-        }
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand){
+        //player.getAbilities().allowFlying = true;
+        player.playSound(SoundEvents.BLOCK_BEACON_ACTIVATE, 1.0f, 1.0f);
+        player.getItemCooldownManager().set(this, 100);
+        return TypedActionResult.success(player.getStackInHand(hand));
 
-        if (bl) {
-            return ActionResult.success(world.isClient);
-        } else {
-            return ActionResult.FAIL;
-        }
-
-    }
-
-    private void playUseSound(World world, BlockPos blockPos) {
-        Random random = world.getRandom();
-        world.playSound((PlayerEntity)null, blockPos, SoundEvents.BLOCK_CONDUIT_ACTIVATE, SoundCategory.BLOCKS, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
     }
 
     @Override
@@ -92,6 +67,7 @@ public class Arkhalitz extends ToolItem implements Vanishable {
     public boolean hasGlint(ItemStack stack) {
         return true;
     }
+
 
     public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
         return !miner.isCreative();
